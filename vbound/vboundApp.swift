4bb039a4175fafcf94d5e08b62c29a96b45d314f
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2026-06-26 01:37:08 +0200

    Initial Commit

---
+++ vbound/vboundApp.swift
@@ -0,0 +1,17 @@
+//
+//  vboundApp.swift
+//  vbound
+//
+//  Created by Adrian Castro on 26/6/26.
+//
+
+import SwiftUI
+
+@main
+struct vboundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
